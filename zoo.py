c5f3b7dcb3cd743251a666b23c3ee4b107718b53
Luis Villén <l.villenjaldon@gmail.com>
2020-11-05 15:06:00 +0100

    Version 0.4

--- zoo.py
+++ zoo.py
@@ -1,20 +1,50 @@
 import screen
 
-def calcularPrecioEntrada(e):
+preciosE = {
+    'bebe': 0.0,
+    'niño': 14.0,
+    'adulto': 23.0,
+    'jubilado': 18.0
+    }
+
+totales = {'bebe':0, 'niño':0, 'adulto':0, 'jubilado'=0}
+
+entradasQ = {
+    'bebe': {
+        'cantidad': (4,15),
+        'precioA': (4,19),
+        },
+    'niño': {
+        'cantidad': (5,15),
+        'precioA': (5,19),
+        },
+    'adulto': {
+        'cantidad': (6,15),
+        'precioA': (6,19),
+        },
+    'jubilado': {
+        'cantidad': (7,15),
+        'precioA': (7,19),
+        }
+    }
+
+def tipoEntrada(e):
     if e > 0 and e <=2:
-        precio = 0
+        tipo = 'bebe'
     elif e <=12:
-        precio = 14
+        tipo = 'niño'
     elif e < 65:
-        precio = 23
+        tipo = 'adulto'
     else:
-        precio = 18
-    return precio
+        tipo = 'jubilado'
+        
+    return tipo
 
 def validaEdad(cadena):
     try:
         n = int(cadena)
         if n>=0:
+            #Entonces el valor es válido
             return True        
         return False
     except:
@@ -25,45 +55,67 @@ def pedirEdad():
     edad = input("¿Qué edad tienes? ")
     while validaEdad(edad) == False:
         print("Edad incorrecta, vuelva a intentarlo")
+        screen.locate(1,1)
         edad = input("¿Qué edad tienes? ")
+        
     return int(edad)
 
+def printScreen():
+    screen.locate(5, 5)
+    print("Bebé....:   -")
+    screen.locate(6, 5)
+    print("Niño....:   -")
+    screen.locate(7, 5)
+    print("Adulto..:   -")
+    screen.locate(8, 5)
+    print("Jubilado:   -")
+    
+    screen.locate(10,8)
+    print("Total...: ")
+    
+    
 screen.clear()
-edad = pedirEdad()
-precioTotal = 0
-linea = 4
+printScreen()
 
-numEntradasB = 0
-numEntradasN = 0
-numEntradasA = 0
-numEntradasJ = 0
+edad = pedirEdad()
+precioTotal = 0 #Negocio
 
 while edad != 0:
-    precioE = calcularPrecioEntrada(edad)
-    if precioE == 0:
-        linea = 4
-        numEntradasB += 1
-        screen.locate(linea, 1)
-        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasB))
-    if precioE == 14:
-        linea = 5
-        numEntradasN += 1
-        screen.locate(linea, 1)
-        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasN))
-    if precioE == 23:
-        linea = 6
-        numEntradasA += 1
-        screen.locate(linea, 1)
-        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasA))
-    if precioE == 18:
-        linea = 7
-        numEntradasJ += 1
-        screen.locate(linea, 1)
-        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasJ))
-    linea += 1
+    tipoE = tipoEntrada(edad)
+    precioE = preciosE[tipoE]
+    screen.locate(entradasQ[tipoE]['cantidad'][0], entradasQ[tipo]['cantidad'][1])
+    print(1)
+    screen.locate(entradasQ[tipoE]['precioA'][0], entradasQ[tipoE]['precioA'][1])
+    print("{:7.2f}€".format(precioE))
     precioTotal += precioE
     
     edad = pedirEdad()
 
-screen.locate(linea, 1)
-print("Total: {} €".format(precioTotal))
+    '''
+        if precioE == 0:
+            scree.locate(4,15)
+            print(1)
+            screen.locate(4,19)
+            print(precioE)
+            
+        if precioE == 14:
+            screen.locate(5,15)
+            print(1)
+            screen.locate(5,19)
+            print(precioE)
+            
+        if precioE == 23:
+            screen.locate(6,15)
+            print(1)
+            screen.locate(6,19)
+            print(precioE)
+            
+        if precioE == 18:
+            screen.locate(7,15)
+            print(1)
+            screen.locate(7,19)
+            print(precioE)
+    '''
+
+screen.locate(linea, 12)
+print("Total: {:7.2f} €".format(precioTotal))
