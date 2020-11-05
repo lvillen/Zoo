fad254af62a7aa9a9ea96933a7eef8b114e04f38
Luis Villén <l.villenjaldon@gmail.com>
2020-11-05 14:54:41 +0100

    versión 0.3

--- zoo.py
+++ zoo.py
@@ -30,14 +30,36 @@ def pedirEdad():
 
 screen.clear()
 edad = pedirEdad()
-
 precioTotal = 0
 linea = 4
 
+numEntradasB = 0
+numEntradasN = 0
+numEntradasA = 0
+numEntradasJ = 0
+
 while edad != 0:
     precioE = calcularPrecioEntrada(edad)
-    screen.locate(linea, 1)
-    print("Precio entrada: {} €".format(precioE))
+    if precioE == 0:
+        linea = 4
+        numEntradasB += 1
+        screen.locate(linea, 1)
+        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasB))
+    if precioE == 14:
+        linea = 5
+        numEntradasN += 1
+        screen.locate(linea, 1)
+        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasN))
+    if precioE == 23:
+        linea = 6
+        numEntradasA += 1
+        screen.locate(linea, 1)
+        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasA))
+    if precioE == 18:
+        linea = 7
+        numEntradasJ += 1
+        screen.locate(linea, 1)
+        print("Precio entrada: {} €\tNum. Entradas: {}".format(precioE, numEntradasJ))
     linea += 1
     precioTotal += precioE
     
